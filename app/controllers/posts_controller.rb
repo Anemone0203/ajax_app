50dc592f98a496f188db67f5e7f603598f7b28d9
Anemone0203 <lady.murasaki@gmail.com>
2020-12-21 12:43:12 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,27 @@
   # @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
   # end
 # end
-
 class PostsController < ApplicationController
+  
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+  
 end
